29d0105e837b64176cc245909a6e7019a2f38f00
Mike Barnes <m.barnes@draftkings.com>
2024-01-12 16:45:06 -0500

    change adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "hybrid",
-  adapter: cloudflare({
-    mode: "directory",
-    imageService: "noop",
-  }),
 });
