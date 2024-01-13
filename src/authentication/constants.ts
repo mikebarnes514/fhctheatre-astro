85c334bf03c8a55c835e7642a9d0fc2f43c6fd14
Mike Barnes <m.barnes@draftkings.com>
2024-01-13 10:29:40 -0500

    update cookie TTL

--- src/authentication/constants.ts
+++ src/authentication/constants.ts
@@ -1,2 +1,2 @@
-export const FHC_COOKIE_KEY = "FHC_THEATRE_TOKEN";
-export const FHC_COOKIE_MAX_AGE_IN_SECONDS = 60 * 60 * 24 * 30; //30 days (seconds -> minutes -> hours -> days)
+export const FHC_COOKIE_KEY = "FHC_THEATRE_TOKEN_MUSICAL";
+export const FHC_COOKIE_MAX_AGE_IN_SECONDS = 60 * 60 * 24 * 90; //90 days (seconds -> minutes -> hours -> days)
